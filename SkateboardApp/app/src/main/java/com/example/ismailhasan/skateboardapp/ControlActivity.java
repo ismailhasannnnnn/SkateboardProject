586c954c37b9f666eb7cc76095181f0c19c1f670
Ismail Hasan <ismailhasan0132@gmail.com>
2018-05-21 10:47:55 -0700

    Created a list view to list all bluetooth paired devices on the phone. From there, you click one, and it takes you to the control page.

--- SkateboardApp/app/src/main/java/com/example/ismailhasan/skateboardapp/ControlActivity.java
+++ SkateboardApp/app/src/main/java/com/example/ismailhasan/skateboardapp/ControlActivity.java
@@ -1,13 +1,32 @@
 package com.example.ismailhasan.skateboardapp;
 
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.UUID;
 
 public class ControlActivity extends AppCompatActivity {
+
+    Button btnForward, btnBackward;
+    String address = null;
+    ProgressDialog progressDialog;
+    BluetoothAdapter myBluetooth = null;
+    BluetoothSocket btSocket = null;
+    private boolean isBtConnected = false;
+    static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control);
+
+        Intent newInt = getIntent();
+        address = newInt.getStringExtra(MainActivity.EXTRA_ADDRESS);
+
     }
 }
--- SkateboardApp/app/src/main/java/com/example/ismailhasan/skateboardapp/MainActivity.java
+++ SkateboardApp/app/src/main/java/com/example/ismailhasan/skateboardapp/MainActivity.java
@@ -1,19 +1,33 @@
 package com.example.ismailhasan.skateboardapp;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothHeadset;
 import android.content.ComponentName;
 import android.content.Intent;
-import android.support.v7.app.AlertDialog;
+import android.location.Address;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
     BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+    Set<BluetoothDevice> pairedDevices;
+    public static String EXTRA_ADDRESS = "device_address";
+    ArrayList list = new ArrayList();
+    boolean btPossible = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +35,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button connectToBoard = (Button)findViewById(R.id.connectToBoard);
+        Button getPairedDevices = (Button)findViewById(R.id.getPairedDevices);
+
+
+        if(btAdapter == null){
+            easyToast("This device does not support Bluetooth features");
+            btPossible = false;
+        }else if(!btAdapter.isEnabled()){
+            btPossible = true;
+            Intent enableBT = new Intent(btAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBT, 1);
+        }
+
+        if(!isBluetoothConnected()){
+            connectToBoard.setText("Connect To Board");
+        }else{
+            connectToBoard.setText("Control Board");
+        }
+
         connectToBoard.setOnClickListener(new View.OnClickListener(){
 
             public void onClick(View v){
@@ -30,14 +62,25 @@ public class MainActivity extends AppCompatActivity {
                         "com.android.settings.bluetooth.BluetoothSettings");
                 intent.setComponent(cn);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                if(isBluetoothConnected()){
+                if(!btPossible){
+                    easyToast("This device does not support Bluetooth features");
+                }else if(!isBluetoothConnected()){
                     startActivity(intent);
                 }else{
-                    easyToast("You're already connected to a device");
+                    easyToast("Connected");
+                    startActivity(new Intent(MainActivity.this, ControlActivity.class));
                 }
+            }
+        });
+
+        getPairedDevices.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                pairedDevices(v);
             }
 
         });
+
     }
 
     public boolean isBluetoothConnected() {
@@ -48,4 +91,32 @@ public class MainActivity extends AppCompatActivity {
     public void easyToast(String s){
         Toast.makeText(MainActivity.this, s, Toast.LENGTH_LONG).show();
     }
+
+    public AdapterView.OnItemClickListener deviceListClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            // Get the device MAC address, the last 17 chars in the View
+            String info = ((TextView) view).getText().toString();
+            String address = info.substring(info.length() - 17);
+            // Make an intent to start next activity.
+            Intent i = new Intent(MainActivity.this, ControlActivity.class);
+            //Change the activity.
+            i.putExtra(EXTRA_ADDRESS, address); //this will be received at ledControl (class) Activity
+            startActivity(i);
+        }
+    };
+
+    public void pairedDevices(View v) {
+        ListView deviceList = (ListView)findViewById(R.id.deviceList);
+        pairedDevices =  btAdapter.getBondedDevices();
+        if(pairedDevices.size() > 0){
+            for(BluetoothDevice bt : pairedDevices){
+                list.add(bt.getName() + "\n" + bt.getAddress());
+            }
+            final ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
+            deviceList.setAdapter(adapter);
+            deviceList.setOnItemClickListener(deviceListClickListener);
+        }
+    }
+
 }
